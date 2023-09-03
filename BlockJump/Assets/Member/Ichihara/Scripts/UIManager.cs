d131883828198b0c8ddbe09de82a23588054d219
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-09-04 01:56:34 +0900

    Create UIManager.cs
    UIのマネージャスクリプトを作成した。

---
+++ BlockJump/Assets/Member/Ichihara/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : SingletonMonoBehaviour<UIManager>
+{
+    // 取った星の数を表示
+    [SerializeField]
+    private TextMeshProUGUI _starCountupText = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(_starCountupText == null)
+        {
+            _starCountupText = GameObject.Find("StarCountupText").GetComponent<TextMeshProUGUI>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
