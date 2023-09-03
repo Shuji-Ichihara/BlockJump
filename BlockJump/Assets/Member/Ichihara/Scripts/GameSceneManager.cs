769f392d3f8ad0b4a4c9a2cc2e7f49e9172dfa82
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-09-04 01:55:40 +0900

    Create GameSceneManager.cs
    ゲームシーンのマネージャースクリプトを作成した。

---
+++ BlockJump/Assets/Member/Ichihara/Scripts/GameSceneManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneManager : SingletonMonoBehaviour<GameSceneManager>
+{
+    // プレイヤーが獲得した星の数
+    private int _starCount = 0;
+    public int StarCount => _starCount;
+    // ゴール判定
+    private bool _isGoal = false;
+    public bool IsGoal => _isGoal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _isGoal = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// プレイヤーがゴールにした時に呼び出す
+    /// </summary>
+    public void PlayerReachesGoal()
+    {
+        _isGoal = true;
+    }
+
+    /// <summary>
+    /// プレイヤーが星を取得した時に呼び出す
+    /// </summary>
+    public void CountupStar()
+    {
+        _starCount++;
+    }
+}
